5cc23ccbb5c4728ec22db61ee5b23ad6be22f46e
ballabollka <goncarovadara454@gmail.com>
2025-05-31 22:13:59 +1000

    Обновлены настройки CORS и email

--- backend/settings.py
+++ backend/settings.py
@@ -130,6 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'https://programmschool.netlify.app'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
