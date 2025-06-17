cb83e572422e8d6412cd9a982efe53c2a6dfebdf
ballabollka <goncarovadara454@gmail.com>
2025-06-17 20:23:40 +1000

    Fix: update API endpoints

--- users/views.py
+++ users/views.py
@@ -6,24 +6,24 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import RegisterSerializer, LoginSerializer
 
-# Класс для регистрации пользователя
+# регистрации пользователя
 class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()  # Создаем пользователя
-            refresh = RefreshToken.for_user(user)  # Генерация токена
+            user = serializer.save()  
+            refresh = RefreshToken.for_user(user)  
             return Response({'token': str(refresh.access_token)}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# Класс для логина пользователя
+# логина пользователя
 class LoginView(APIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.validated_data  # Получаем аутентифицированного пользователя
-            refresh = RefreshToken.for_user(user)  # Генерация токена
-            access_token = str(refresh.access_token)  # Получаем access токен
+            user = serializer.validated_data  
+            refresh = RefreshToken.for_user(user)  
+            access_token = str(refresh.access_token) 
             return Response({
                 'access': access_token,
                 'refresh': str(refresh)
